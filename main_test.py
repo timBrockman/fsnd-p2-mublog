f028daba17807f0000b2e3f0f69505e8112f4970
Tim Brockman <tim@timbrockman.com>
2017-04-10 16:51:53 -0400

    re-add tests

--- main_test.py
+++ main_test.py
@@ -5,7 +5,7 @@ so I'm flying blind on many parts
 """
 import unittest
 #import main
-from app.hasher import hash_this, check_hash
+from app.hasher import gen_salt, hash_pw, check_pw, hash_this, check_hash
 
 '''class SanityTest(unittest.TestCase):
     """testing sanity"""
@@ -23,6 +23,27 @@ class HashesTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
+    #pw hashers
+    def test_gen_salt(self):
+        """gen_salt() returns str len 8"""
+        self.assertEqual(len(gen_salt()), 8)
+
+    def test_hash_pw(self):
+        """hash_pw returns str len 72"""
+        test_hashed_pw = hash_pw('bob', 'test pw')
+        self.assertEqual(len(test_hashed_pw), 72)
+
+    def test_check_pw_true(self):
+        """check_pw() returns true when it should"""
+        test_hashed_pw = hash_pw('bob', 'test pw')
+        self.assertTrue(check_pw('bob', 'test pw', test_hashed_pw))
+
+    def test_check_pw_false(self):
+        """check_pw() returns false when it should"""
+        test_hashed_pw = hash_pw('bob', 'test pw')
+        self.assertFalse(check_pw('bob', 'bad pw', test_hashed_pw))
+
+    #cookie hashers
     def test_hash_this_len_32(self):
         """hash_this returns str len 32"""
         test_str = 'test string * stuff'
